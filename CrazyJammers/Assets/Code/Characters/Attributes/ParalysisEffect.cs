127c2f63b0f7b918171fa4f12001dc322b7bd4ce
malz414 <72981910+malz414@users.noreply.github.com>
2025-12-03 01:55:04 +0000

    Update ParalysisEffect.cs

--- CrazyJammers/Assets/Code/Characters/Attributes/ParalysisEffect.cs
+++ CrazyJammers/Assets/Code/Characters/Attributes/ParalysisEffect.cs
@@ -9,8 +9,8 @@ public class ParalysisEffect
     private int turnsRemaining;
     private bool isActivatedThisTurn;
 
-    private const float PARALYSIS_EFFECT_CHANCE = 1f;
-    private const float PARALYSIS_EFFECT_ENHANCED_CHANCE = 1f;
+    private const float PARALYSIS_EFFECT_CHANCE = .40f;
+    private const float PARALYSIS_EFFECT_ENHANCED_CHANCE = 0.6f;
 
     private bool isEnhancedParalysis = false;
 
