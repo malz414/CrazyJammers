290c361c04d7472754d011fbc0921a5615933199
malz414 <72981910+malz414@users.noreply.github.com>
2025-12-03 01:44:44 +0000

    100 chance to para

--- CrazyJammers/Assets/Code/Characters/Attributes/ParalysisEffect.cs
+++ CrazyJammers/Assets/Code/Characters/Attributes/ParalysisEffect.cs
@@ -9,8 +9,8 @@ public class ParalysisEffect
     private int turnsRemaining;
     private bool isActivatedThisTurn;
 
-    private const float PARALYSIS_EFFECT_CHANCE = .15f;
-    private const float PARALYSIS_EFFECT_ENHANCED_CHANCE = 0.6f;
+    private const float PARALYSIS_EFFECT_CHANCE = 1f;
+    private const float PARALYSIS_EFFECT_ENHANCED_CHANCE = 1f;
 
     private bool isEnhancedParalysis = false;
 
