ad4272bc3ff4ecf7bdaa248ba9b768fafa45eb1c
ddoodledev <155783140+ddoodledev@users.noreply.github.com>
2026-04-28 20:44:24 -0500

    Fixed Heal and Heal Timing for Boss, Fixed X Position Offset for IceHit, Finished Ui Splicingt

--- CrazyJammers/Assets/Code/Characters/TurnManager.cs
+++ CrazyJammers/Assets/Code/Characters/TurnManager.cs
@@ -564,7 +564,7 @@ public class TurnManager : MonoBehaviour
                         case 3: yRotationOffset = -110f; break;
                     }
                     ApplyEffectWithDelay(iceAttack, enemy.transform, 0f, 3.0f, null, 2f, yRotationOffset);
-                    ApplyEffectWithDelay(iceHit, hero.transform, .2f, 4f, null, .5f);
+                    ApplyEffectWithDelay(iceHit, hero.transform, .3f, 3.5f, null, 2f, null,null,0.78f);
 
                 }
 
@@ -1274,7 +1274,7 @@ public class TurnManager : MonoBehaviour
                 blurbEvent.Set($"{damage} Health Recovered!");
                 EventBus.Publish(blurbEvent);
                 usedMove1.text = $"Boss attacked {attackNamesText} with {combinedAttack.attackName} and recovered {damage} Health!";
-                ApplyEffectWithDelay(heal, hero.transform, 0f, 3.0f);
+                ApplyEffectWithDelay(heal, hero.transform, 0f, 2.0f);
                 EventBus.Publish(statusUpdateEvent);
             }
 
@@ -1316,7 +1316,7 @@ public class TurnManager : MonoBehaviour
                 blurbEvent.Set($"Status Healed");
                 EventBus.Publish(blurbEvent);
                 usedMove1.text = $"Status Healed";
-                ApplyEffectWithDelay(healfield, hero.transform, 0f, 3.0f, true, 0.10f, null, true);
+                ApplyEffectWithDelay(healfield, hero.transform, 0f, 2.0f, true, 0.10f, null, true);
                 ApplyEffectWithDelay(panaceaVFX, hero.transform, 0f, 3.0f);
             }
 
@@ -1397,7 +1397,7 @@ public class TurnManager : MonoBehaviour
                     }
 
                     ApplyEffectWithDelay(iceAttack, hero.transform, 0f, 3.0f, null, 1.5f, yRotationOffset); 
-                    ApplyEffectWithDelay(iceHit, hero.transform, 0.2f, 4.0f, null, 0.5f, null, null, 1f, 0f);
+                    ApplyEffectWithDelay(iceHit, targetEnemy.transform, 0.3f, 4.0f, null, 1f, null, null, 0.25f);
                 }
                 
                 blurbEvent.Set($"You strike again");
